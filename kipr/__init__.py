42f284d8477b285b8225114a2a655b918580a872
Kipre <kipr@live.ru>
2021-02-10 18:15:04 +0100

    simple namespace magic

--- kipr/__init__.py
+++ kipr/__init__.py
@@ -1,5 +1,7 @@
+import types
 from kipr_array import *
 from . import unicode_simplify
 from . import sys_utils as sys
 
-text = type('k_module', (object,), {'normalization_map': unicode_simplify.normalize})()
+text = types.SimpleNamespace()
+text.normalization_map = unicode_simplify.normalize
